7abd194b6488df8c272a67cc0d23459872a06b48
Hugo McNally <hugo.mcnally@gmail.com>
2019-08-25 21:01:09 +0100

    misc/link.sh

---
+++ misc/link.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+echo "{" > index.json
+echo "    \"collection\": [" >> index.json
+
+for file in  ~/Videos/YetToWatch/*.mp4
+do
+    name="$(basename "$file")"
+    name_no_ext="$(basename -s .mp4 "$name")"
+    u_name="$(basename "${file// /_}")"
+    u_name_no_ext="$(basename -s .mp4 "$u_name")"
+    
+    mkdir "$u_name_no_ext"
+    ln -s "$file" "$u_name_no_ext"/"$u_name"
+
+    echo "        {" >> index.json
+    echo "            \"id\": \"$u_name_no_ext\"," >> index.json
+    echo "            \"name\": \"$name_no_ext\"," >> index.json
+    echo "            \"year\": 0," >> index.json
+    echo "            \"location\": \"$u_name_no_ext/$u_name\"," >> index.json
+    echo "            \"poster\": \"\"" >> index.json
+    echo "        }," >> index.json
+done
+echo "    ]" >> index.json
+echo "}" >> index.json
+
+#for file in *' '*
+#do
+#  if [ -e "${file// /_}" ]
+#  then
+#    echo Skipping "$file" as the "${file// /_}" already exists
+#    continue
+#  fi
+#
+#  mv "$file" "${file// /_}"
+#done
+#
+#rm ../files/films.txt
+#
+#for file in *
+#do
+#    echo "$file" >> ../files/films.txt
+#done
